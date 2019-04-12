dd621696a0bbc3f642b95dc98e90fae5e3174759
Ofirnir <Ofirnir@users.noreply.github.com>
2019-04-12 18:01:01 +0300

    Created the simple tagger

--- venv/Include/ex2.py
+++ venv/Include/ex2.py
@@ -1,3 +1,13 @@
 import numpy as np
-import viterbi
+import simple_tagger as st
+import nltk
+nltk.download('treebank')
 from nltk.corpus import treebank
+print(len(treebank.tagged_sents()))
+#output: 3914
+train_data = treebank.tagged_sents()[:3000]
+test_data = treebank.tagged_sents()[3000:]
+# print (train_data[0])
+x = st.simple_tagger()
+x.train(train_data)
+print(x.evaluate(test_data))
---
+++ venv/Include/simple_tagger.py
@@ -0,0 +1,30 @@
+from collections import defaultdict,Counter
+
+class simple_tagger:
+    def __init__(self) :
+        self.mapping = defaultdict()
+
+    def train(self,data):
+        counter = Counter()
+        ## Count words
+        for sentence in data:
+            for word in sentence:
+                counter[word] += 1
+        ### iterating over ordered list and adding the most commons tags
+        for count in counter.most_common():
+            tag = count[0]
+            if tag[0] not in  self.mapping:
+                self.mapping[tag[0]] = tag[1]
+        return self.mapping
+
+    def evaluate(self,data):
+        count = 0
+        word_level_acc = 0.0
+        for sentence in data:
+            for word in sentence:
+                count+=1
+                if word[0]  in self.mapping:
+                    if self.mapping[word [0]] == word[1] : word_level_acc += 1
+                else: self.mapping[word[0]] = word[1]
+
+        return word_level_acc / count
